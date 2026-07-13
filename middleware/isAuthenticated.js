141c545ba9ae31bc1b872ee3b93cc2aa309708dd
shubhg17 <shubamghai116@gmail.com>
2026-07-14 01:01:23 +0530

    Completed User Business Logic

--- middleware/isAuthenticated.js
+++ middleware/isAuthenticated.js
@@ -3,7 +3,7 @@ const isAuthenticated = async (req , res , next)=>  {
   try {
     const token = req.cookies?.token
     if(!token) return res.status(401).json({message: "User not authenticated"})
-      //jwt.verif() isme 2 params pass kro first apna token and apni secret key secret key se hi basically yeh verify krta hain
+      //jwt.verify() isme 2 params pass kro first apna token and apni secret key secret key se hi basically yeh verify krta hain
     const decode = await jwt.verify(token , process.env.JWT_SECRET_KEY)
     if(!decode) return res.status(401).json({message: "Invalid Token"})
     //agar match hoga toh yaha aoge and yeh decode kya contain krega hamare token ke data basically joh payload ki humne token me kya data diya tha 

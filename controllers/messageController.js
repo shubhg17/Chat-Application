b317356cdbada8bbca89e17c326afe23876e629d
shubhg17 <shubamghai116@gmail.com>
2026-07-14 20:03:31 +0530

    Completed GetUser Endpoint

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -10,9 +10,13 @@ export const sendMessage = async(req , res) => {
    //sabse phele hum users ke beech me communication establish krenge
 
    //with this we are finding ki conversation kis kis ke beech me chl rhi hain 
+   //iss se hum check kr rhe hai ki do users ke beech me conversation pehle se hui toh nhi hain 
    let gotConversation = await Conversation.findOne({
     //iss se tum apne participations vali array me find kroge joh tumne conversationmodel me banayi thi here $all yeh operator kya krta hain ki array me joh bhi id store hogi voh id se find krdega basically jitni bhi ids store hongi uss array me 
-       participants:{$all : [senderId , receiverId]}
+    //$in lagayenge toh voh senderID or recevierId yeh ese dhundega aur agar ek bhi milgyi toh document return krdega but hume dono chaiye isliye $all use krenge "participants wali array me ye dono IDs honi chahiye."$all ko order se koi matlab nahi.bas dono milni chaiye order kuch bhi ho 
+       participants:{
+        $all : [senderId , receiverId]
+      }
    })
     
    //ki agar conversation nhi hogi nah tabhi basically tum conversation ek create kroge and iss variable me change krdenge tbhi let use kiya not const
@@ -33,6 +37,7 @@ export const sendMessage = async(req , res) => {
       gotConversation.messages.push(newMessage._id)
    }
 
+   //Jo changes maine gotConversation object me kiye hain, unhe database me bhi update kar do as gotConversation document ki ek copy ha joh hum gotConversation variable me hold kr rhe hain 
    await gotConversation.save()
 
    //After above now here we will implement SOCKET.IO
@@ -43,4 +48,22 @@ export const sendMessage = async(req , res) => {
     console.log(error)
   }
   
+}
+
+//abh function joh hum likhenge voh hoga message ko get krne ke liye
+
+export async function getMessage(req , res) {
+   try {
+       const receiverId = req.params.id
+       const senderId = req.id
+       const conversation = await Conversation.findOne({
+           participants:{$all : [senderId , receiverId]}
+       }).populate("messages")
+
+       //this conversation?.messages means ki agar hume conversation model me sender aur receiver ki convo mil jati hain then hum kya krenge unke sare messages ko hum return krdenge as response 
+       return res.status(200).json(conversation?.messages)
+   }
+   catch(error) {
+     console.log(error)
+   }
 }
--- routes/messageRoute.js
+++ routes/messageRoute.js
@@ -1,8 +1,9 @@
 import express from "express"
-import {sendMessage} from "../controllers/messageController.js"
+import {sendMessage , getMessage} from "../controllers/messageController.js"
 import isAuthenticated from "../middleware/isAuthenticated.js"
 const router = express.Router()
 
 router.post("/send/:id" , isAuthenticated , sendMessage)
+router.get("/:id" , isAuthenticated , getMessage)
 
 export default router;

1fa44869fdbbc0c2d3e06097258ee21aaf3f01ae
shubhg17 <shubamghai116@gmail.com>
2026-07-15 18:39:42 +0530

    Added frontend and reorganized project structure

---
+++ frontend/components/SignUp.jsx
@@ -0,0 +1,11 @@
+//to install react we used npx create-react-app . here . means ki current folder mtlb frontend me install krdo
+
+import React from "react"
+
+const SignUp = () => {
+   return (
+     <div>SignUp</div>
+   )
+}
+
+export default SignUp
---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       Chat Application 
+    </div>
+  );
+}
+
+export default App;
